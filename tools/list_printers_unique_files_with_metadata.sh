080c528162cd58688e7aa7fae38b1bc428e47934
Thomas Debesse <dev@illwieckz.net>
2014-05-13 22:30:02 +0200

    little mistake

--- tools/list_printers_unique_files_with_metadata.sh
+++ tools/list_printers_unique_files_with_metadata.sh
@@ -15,9 +15,13 @@ archives_list="${WORKSPACE}/lists/printers_archives_with_licenses.txt"
 
 ! [ -f "${archives_list}" ] && echo "${error_message}" && exit 1
 
-cat "${WORKSPACE}/lists/printers_files_with_metadata.txt" | cut -c 1-32 | sort | uniq | while read file_sum
-do
-	grep "^${file_sum}" "${WORKSPACE}/lists/printers_files_with_metadata.txt" | sort -k 3 | tee "${WORKSPACE}/lists/printers_unique_files_with_metadata.txt"
-done
+function unique_list {
+	cat "${WORKSPACE}/lists/printers_files_with_metadata.txt" | cut -c 1-32 | sort | uniq | while read file_sum
+	do
+		grep "^${file_sum}" "${WORKSPACE}/lists/printers_files_with_metadata.txt" 
+	done
+}
+
+unique_list | sort -k 3 | tee "${WORKSPACE}/lists/printers_unique_files_with_metadata.txt"
 
 #EOF
