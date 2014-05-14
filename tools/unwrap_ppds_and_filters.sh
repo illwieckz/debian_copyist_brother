089b1306563d5c06483a860eee6b9373306db05e
Thomas Debesse <dev@illwieckz.net>
2014-05-14 20:54:28 +0200

    tool to unwrap ppds and filters

---
+++ tools/unwrap_ppds_and_filters.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+if [ "${WEBBROTHER_WORKSPACE}" != '' ]
+then
+	WORKSPACE="${WEBBROTHER_WORKSPACE}"
+else
+	WORKSPACE="$(cd "$(dirname $0)"; pwd)/../material/"
+fi
+
+modified_cupswrapper_dir="${WORKSPACE}/abstract/cupswrappers_modified/"
+unwrap_dir="${WORKSPACE}/abstract/generated_content_original/"
+mkdir -p "${modified_cupswrapper_dir}"
+mkdir -p "${unwrap_dir}"
+
+cd "${unwrap_dir}";
+mkdir -p "ppds" "filters";
+for s in "${modified_cupswrapper_dir}"*
+do
+	echo "./${s}"
+done
+cd - >/dev/null
+
+#EOF
